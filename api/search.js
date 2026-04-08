5bf33c421eb7c8344556d37aed6530d654bde55a
Mohandessam69 <mohandessam99me@gmail.com>
2026-04-08 09:50:36 +0200

    Add search handler for Google Shopping API
    Implement search handler to fetch shopping results from SerpAPI.

---
+++ api/search.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const { query } = req.query;
+  
+  const response = await fetch(
+    `https://serpapi.com/search.json?engine=google_shopping&q=${encodeURIComponent(query)}&api_key=${process.env.SERPAPI_KEY}`
+  );
+  
+  const data = await response.json();
+  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.json(data.shopping_results || []);
+}
